8515f9e2147349613126b2d7854cff29f88fa7af
DESKTOP-1VHAMA3\PTVTELECOM <notiene@gmail.com>
2026-04-10 13:45:39 +0200

    Añadimos la nueva ruta para el método buscar()

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ require __DIR__.'/auth.php';
 
 
 Route::get('/ideas',[IdeaController::class, 'index'])-> name('idea.index');
+Route::get('/ideas/buscar',[IdeaController::class, 'buscar'])-> name('idea.buscar');
 Route::get('/ideas/crear',[IdeaController::class, 'create'])-> name('idea.create');
 Route::post('/ideas/crear',[IdeaController::class, 'store'])-> name('idea.store');
 Route::get('/ideas/editar/{idea}',[IdeaController::class, 'edit'])-> name('idea.edit');
