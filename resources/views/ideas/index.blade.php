9f3bc33d823212544847f9d5b83f5a0fa36dfc8f
DESKTOP-1VHAMA3\PTVTELECOM <notiene@gmail.com>
2026-04-13 12:26:41 +0200

    Creación de un selector para filtrar el tipo de busqueda en la BBDD por título, autor o descripción.

--- resources/views/ideas/index.blade.php
+++ resources/views/ideas/index.blade.php
@@ -33,6 +33,14 @@
 <div id = "buscador" class="flex justify-end items-center mb-4">
     <form action="{{ route('idea.buscar') }}" method="GET" >
         <input type="text"  name ="buscar" placeholder="Buscar">
+
+        <select name="selector" >
+            <option value="">Elige una opción</option>
+            <option value="autor">Autor</option>
+            <option value="titulo">Título</option>
+            <option value="descripcion">Descripción</option>
+        </select>
+
         <button type="submit" class="px-4 py-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-sm text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700">Buscar</button>
         
     </form>
